77f1bab55a94c6315e3580826fe04d1503fd34bb
admin-313 <w@quantum-level.net>
2025-01-22 18:58:14 +0200

    Base auth module

---
+++ src/auth/auth_middleware.py
@@ -0,0 +1,11 @@
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject, Message
+from typing import Any, Callable, Awaitable
+
+class AuthMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any]
+    ) -> Any:
---
+++ src/auth/router.py
@@ -0,0 +1,3 @@
+from aiogram import Router
+
+middleware_router = Router()
