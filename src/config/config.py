f5db04ed6cb30ed2453f73bc895d247f538b287c
admin-313 <w@quantum-level.net>
2025-01-24 14:30:48 +0200

    Users config reader

--- src/config/config.py
+++ src/config/config.py
@@ -1,17 +1,41 @@
-import pathlib
-
-from pydantic import TypeAdapter
+import logging
+from pathlib import Path
+from pydantic import TypeAdapter, ValidationError
 from config.schemas import User
+from config.exceptions import (
+    WhitelistConfigFileNotFound,
+    WhitelistConfigValidationError,
+)
 
-class JSONConfigReader():
+USERS_CONFIG_PATH: Path = Path(__file__).parent / "db" / "users.json"
 
-    def __init__(self) -> None:
-        pass
+logger = logging.getLogger(__name__)
 
-    def get_whitelisted_users() -> list[User]:
-        pass
-            
-    def _get_users_config() -> str:
-        json_string: str = 
 
-class JSONConfigWriter():
+class JSONConfigReader:
+
+    def get_whitelisted_users(self) -> list[User]:
+        content: str = self._get_users_db_content()
+        user_list_adapter = TypeAdapter(list[User])
+        try:
+            return user_list_adapter.validate_json(content)
+
+        except ValidationError as ve:
+            logger.error(f"Could not validate users config {str(ve)}")
+            raise WhitelistConfigValidationError(
+                "The config is likely to be empty or contain invalid data"
+            )
+
+    def _get_users_db_content(self) -> str:
+        try:
+            return USERS_CONFIG_PATH.read_text()
+
+        except FileNotFoundError:
+            logger.critical(f"The users.json config file doesn't seem to exist")
+            raise WhitelistConfigFileNotFound(
+                "The config file doesn't seem to exist. Have you renamed it to users.json?"
+            )
+
+
+class JSONConfigWriter:
+    pass
