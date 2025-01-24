f7b2aa363f25037dc4346b41601b3545ba2ff204
admin-313 <w@quantum-level.net>
2025-01-24 11:39:01 +0200

    Change id datatype to be more strict

--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt
 from datetime import datetime
 
 class User(BaseModel):
-    id: int
+    id: PositiveInt
     member_since: datetime
     is_superuser: bool
     last_known_name: str
