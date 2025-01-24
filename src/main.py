4c2c25509fc98ed8386367e46a747fefd888977f
admin-313 <w@quantum-level.net>
2025-01-24 14:30:23 +0200

    entry point skeleton

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import logging
+import asyncio
+
+async def main() -> None:
+    logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+
+if __name__ == "__main__":
+    asyncio.run(main())
