1cc2e0ba2c7e7f8aa6c72b8a8576359c201c329a
admin-313 <w@quantum-level.net>
2025-01-27 14:41:17 +0200

    Separate config declarations into different functions

--- src/main/config.py
+++ src/main/config.py
@@ -1,20 +1,55 @@
 import os
 import logging
 from pydantic import ValidationError
-from main.exceptions import ConfigurationException
-from main.schemas import BotConfiguration
+from main.exceptions import (
+    BotTokenConfigurationException,
+    LinkToFbParserConfigurationException,
+    AdminTelegramIdConfigurationException,
+)
+from main.schemas import (
+    BotConfiguration,
+    AdminTelegramIdConfiguration,
+    LinkToFbParserConfiguration,
+)
 
 logger = logging.getLogger(__name__)
 
-async def main() -> None:
 
+def get_bot_configuration() -> BotConfiguration:
     try:
-        config = BotConfiguration(
-            link_to_fb_parser=os.getenv("LINK_TO_FB_PARSER"), # type: ignore
-            admin_telegram_id=os.getenv("ADMIN_TELEGRAM_ID"), # type: ignore
-            bot_token=os.getenv("BOT_TOKEN"), # type: ignore
+        return BotConfiguration(
+            bot_token=os.getenv("BOT_TOKEN"),  # type: ignore
+        )
+    except ValidationError as ve:
+        logger.critical(f"Could not load bot configurational variables: {str(ve)}")
+        raise BotTokenConfigurationException(
+            "Have you defined all env variables needed?"
         )
 
+
+def get_link_to_fb_parser_configuration() -> LinkToFbParserConfiguration:
+    try:
+        return LinkToFbParserConfiguration(
+            link_to_fb_parser=os.getenv("LINK_TO_FB_PARSER")  # type: ignore
+        )
     except ValidationError as ve:
-        logger.critical(f"Could not load configurational variables: {str(ve)}")
-        raise ConfigurationException("Have you defined all env variables needed?")
+        logger.critical(
+            f"Could not load facebook parser configurational variables: {str(ve)}"
+        )
+        raise LinkToFbParserConfigurationException(
+            "Have you defined all env variables needed?"
+        )
+
+
+def get_admin_telegram_id_configuration() -> AdminTelegramIdConfiguration:
+    try:
+        return AdminTelegramIdConfiguration(
+            link_to_fb_parser=os.getenv("ADMIN_TELEGRAM_ID")  # type: ignore
+        )
+    except ValidationError as ve:
+        logger.critical(
+            f"Could not load admin telegram id configurational variables: {str(ve)}"
+        )
+        raise AdminTelegramIdConfigurationException(
+            "Have you defined all env variables needed?"
+        )
