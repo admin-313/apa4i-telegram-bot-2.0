980376b58bd7a006fe69255c1796e853ca10c0ea
admin-313 <w@quantum-level.net>
2025-01-27 14:04:21 +0200

    Move the core of the app to the main module

--- src/main/main.py
+++ src/main/main.py
@@ -13,13 +13,11 @@ async def main() -> None:
     logger = logging.getLogger(__name__)
 
     try:
-        # Pylance ignore. The purpouse of this string is to check whether
-        # the config_data contains correct values
         config = BotConfiguration(
             link_to_fb_parser=os.getenv("LINK_TO_FB_PARSER"), # type: ignore
             admin_telegram_id=os.getenv("ADMIN_TELEGRAM_ID"), # type: ignore
             bot_token=os.getenv("BOT_TOKEN"), # type: ignore
-        )  # type: ignore
+        )
 
     except ValidationError as ve:
         logger.critical(f"Could not load configurational variables: {str(ve)}")
