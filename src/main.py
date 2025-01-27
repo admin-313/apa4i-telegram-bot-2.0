87451ccdc965fcf3556eab40f881314bc99b629b
admin-313 <w@quantum-level.net>
2025-01-27 15:21:24 +0200

    Add logging configurations

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
+import logging
 import asyncio
 from aiogram import Bot
 from bot.bot import get_dispatcher
 from config.config import get_bot_configuration
+
+logging.basicConfig(format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p")
+logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
     bot_config = get_bot_configuration()
     dp = get_dispatcher()
     bot = Bot(bot_config.bot_token)
-    await dp.start_polling(bot) # type: ignore
+    await dp.start_polling(bot)  # type: ignore
 
 
 if __name__ == "__main__":
