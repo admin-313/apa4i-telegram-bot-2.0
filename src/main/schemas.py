fc639c509626c5d7f2cc05dc612e4f955926f410
admin-313 <w@quantum-level.net>
2025-01-27 14:17:33 +0200

    Split config model into different models

--- src/main/schemas.py
+++ src/main/schemas.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
 
+
 class BotConfiguration(BaseModel):
     bot_token: str
+
+
+class AdminTelegramIdConfiguration(BaseModel):
     admin_telegram_id: str
+
+
+class LinkToFbParserConfiguration(BaseModel):
     link_to_fb_parser: str
