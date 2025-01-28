cf807291e06406f38073a3e6bdca222f6c648129
admin-313 <w@quantum-level.net>
2025-01-28 19:00:58 +0200

    Middleware constructor

--- src/auth/auth_middleware.py
+++ src/auth/auth_middleware.py
@@ -1,9 +1,14 @@
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, Message
 from typing import Any, Callable, Awaitable
+from whitelist.service import JSONConfigReader
 
 
 class AuthMiddleware(BaseMiddleware):
+
+    def __init__(self, json_reader: JSONConfigReader) -> None:
+        self.json_reader = json_reader
+
     async def __call__(
         self,
         handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
--- src/auth/router.py
+++ src/auth/router.py
@@ -1,3 +1,8 @@
 from aiogram import Router
+from auth.auth_middleware import AuthMiddleware
+from whitelist.service import JSONConfigReader
 
+# TODO Переместить ридеры конфигов в auth модуль
 middleware_router = Router()
+
+middleware_router.message.outer_middleware(AuthMiddleware(JSONConfigReader()))
