9fdff6f518a0b53c1d79a848df4185ed269111c0
admin-313 <w@quantum-level.net>
2025-01-28 16:48:01 +0200

    Add code formatting to the unique text

--- src/invisible_text/router.py
+++ src/invisible_text/router.py
@@ -2,6 +2,7 @@ from aiogram import F, Router
 from aiogram.types.message import Message
 from aiogram.types.callback_query import CallbackQuery
 from aiogram.fsm.context import FSMContext
+from aiogram.utils.formatting import Code, Text
 from invisible_text.callbacks import InvisibleTextCallback
 from src.invisible_text.fsm_states import InvisibleTextForm
 from invisible_text.buttons import (
@@ -34,18 +35,19 @@ async def repeat_invisible_text_callback(
     message = callback.message
     if type(message) is Message and message.text:
         await state.set_state(InvisibleTextForm.give_your_text)
-        await message.edit_text(
-            message.text + "\nВведите новый текст для преобразования:"
-        )
+
+        text_content = Text(Code(message.text), "\n\n", "Введите текст для преобразования")
+        await message.edit_text(**text_content.as_kwargs())
         await message.edit_reply_markup(reply_markup=get_invisible_text_on_repeat())
 
 
 @invisible_text_router.message(InvisibleTextForm.give_your_text, F.text)
 async def return_invisible_test(message: Message, state: FSMContext) -> None:
     if message.text:
+        text_content = Text(Code(get_unique_text(message.text)))
         await message.reply(
-            text=get_unique_text(message.text),
             reply_markup=get_invisible_text_on_success(),
+            **text_content.as_kwargs()
         )
         await state.clear()
     else:
