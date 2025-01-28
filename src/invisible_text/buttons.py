9ba6e4372c242b96a970d5adfa8d154b7cdac776
admin-313 <w@quantum-level.net>
2025-01-28 12:00:51 +0200

    Fix callback button

--- src/invisible_text/buttons.py
+++ src/invisible_text/buttons.py
@@ -1,10 +1,15 @@
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+from bot.callbacks import RootCallback
 
 
 def get_invisible_text_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="❌Отмена", callback_data="main_menu")]
+            [
+                InlineKeyboardButton(
+                    text="❌Отмена", callback_data=RootCallback(action="root").pack()
+                )
+            ]
         ]
     )
