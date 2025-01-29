5f7fc6cfbbd241fc2bb7b973c8f6ae280c4599c5
admin-313 <w@quantum-level.net>
2025-01-29 17:51:29 +0200

    Add logging

--- src/auth/auth_middleware.py
+++ src/auth/auth_middleware.py
@@ -1,8 +1,11 @@
+import logging
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject, Message
 from typing import Any, Callable, Awaitable
 from auth.database.json_driver.drivers import JSONConfigReader
 from auth.schemas import User
+
+logger = logging.getLogger(__name__)
 
 
 class AuthMiddleware(BaseMiddleware):
@@ -18,5 +21,9 @@ class AuthMiddleware(BaseMiddleware):
     ) -> Any:
         if isinstance(event, Message) and event.from_user:
             whitelisted_users: list[User] = self.json_reader.get_whitelisted_users()
-            if event.from_user.id in [user.id for user in whitelisted_users]:
+            request_user_id: int = event.from_user.id
+            if request_user_id in [user.id for user in whitelisted_users]:
+                logger.info(f"Request from id {request_user_id} has been accepted")
                 return await handler(event, data)
+            else:
+                logger.info(f"Request from id {request_user_id} has been ignored")
