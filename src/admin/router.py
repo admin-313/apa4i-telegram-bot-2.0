0f6e9be8b7b9206a2b82d4da2d919f11fba09de3
admin-313 <w@quantum-level.net>
2025-01-30 12:55:50 +0200

    admin authorisation

--- src/admin/router.py
+++ src/admin/router.py
@@ -1,4 +1,18 @@
+import logging
+from aiogram.filters.command import Command
+from aiogram.types.message import Message
+from aiogram import Router, F
 from auth.admin_auth_middleware import AdminAuthMiddleware
-from aiogram import Router
+from auth.database.json_driver.drivers import JSONConfigReader
+from auth.database.dispatcher import database_service_dispatcher
 
+logger = logging.getLogger(__name__)
+
+config_reader: JSONConfigReader = database_service_dispatcher["json_readonly"]()
 admin_commands_router = Router()
+admin_commands_router.message.middleware(AdminAuthMiddleware(config_reader))
+
+
+@admin_commands_router.message(F.text, Command("add", prefix="/"))
+async def process_get_command(message: Message) -> None:
+    logger.info("Process get command")
--- src/root/bot.py
+++ src/root/bot.py
@@ -3,11 +3,17 @@ from aiogram import Dispatcher
 from root.router import root_router
 from invisible_text.router import invisible_text_router
 from auth.router import auth_middleware_router
+from admin.router import admin_commands_router
 
 logger = logging.getLogger(__name__)
 
 
 def get_dispatcher() -> Dispatcher:
     dp = Dispatcher()
-    dp.include_routers(auth_middleware_router, root_router, invisible_text_router)
+    dp.include_routers(
+        auth_middleware_router,
+        admin_commands_router,
+        root_router,
+        invisible_text_router,
+    )
     return dp
