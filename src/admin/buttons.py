9a1f11a06efed1035c1f4d445be825dbc32641aa
admin-313 <w@quantum-level.net>
2025-02-03 10:54:50 +0200

    Buttons for the whitelist get get command

---
+++ src/admin/buttons.py
@@ -0,0 +1,41 @@
+from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
+from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+
+
+def get_paginator_first_page_markup() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="▶️", callback_data="next_page"
+                )
+            ]
+        ]
+    )
+
+
+def get_paginator_last_page_markup() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="◀️", callback_data="previous_page"
+                )
+            ]
+        ]
+    )
+
+
+def get_paginator_default_page_markup() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="▶️", callback_data="next_page"
+                ),
+                InlineKeyboardButton(
+                    text="◀️", callback_data="previous_page"
+                ),
+            ]
+        ]
+    )
