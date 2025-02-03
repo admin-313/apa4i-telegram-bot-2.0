5160200b807a95f5b0f8114018a56034df42f302
admin-313 <w@quantum-level.net>
2025-02-03 10:57:17 +0200

    Formatting

--- src/admin/buttons.py
+++ src/admin/buttons.py
@@ -4,24 +4,14 @@ from aiogram.types.inline_keyboard_button import InlineKeyboardButton
 
 def get_paginator_first_page_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="▶️", callback_data="next_page"
-                )
-            ]
-        ]
+        inline_keyboard=[[InlineKeyboardButton(text="▶️", callback_data="next_page")]]
     )
 
 
 def get_paginator_last_page_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="◀️", callback_data="previous_page"
-                )
-            ]
+            [InlineKeyboardButton(text="◀️", callback_data="previous_page")]
         ]
     )
 
@@ -30,12 +20,8 @@ def get_paginator_default_page_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(
-                    text="▶️", callback_data="next_page"
-                ),
-                InlineKeyboardButton(
-                    text="◀️", callback_data="previous_page"
-                ),
+                InlineKeyboardButton(text="▶️", callback_data="next_page"),
+                InlineKeyboardButton(text="◀️", callback_data="previous_page"),
             ]
         ]
     )
