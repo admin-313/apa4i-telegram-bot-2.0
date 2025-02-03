a3a17f614eefd504bc171782f25e0ec49935660c
admin-313 <w@quantum-level.net>
2025-02-03 12:33:49 +0200

    Fix logic operation. Replace raise page not found with assigning target page with the last page

--- src/admin/paginator/paginator.py
+++ src/admin/paginator/paginator.py
@@ -1,6 +1,6 @@
 import math
 from typing import Any
-from admin.paginator.exceptions import PageCantBeZero, PageNotFound
+from admin.paginator.exceptions import PageCantBeZero
 from admin.paginator.schemas import PaginatorResponce
 from auth.database.json_driver.drivers import JSONConfigReader
 
@@ -17,17 +17,16 @@ class Paginator:
         if not target_page:
             raise PageCantBeZero("Page can't be zero")
         if target_page > max_pages:
-            raise PageNotFound("We don't have that many pages")
-        
+            target_page = max_pages
+
         start_index: int = (target_page - 1) * self.elements_per_page
         end_index: int = target_page * self.elements_per_page
 
         return PaginatorResponce(
             page_elements=all_elements[start_index:end_index],
-            is_next_page=target_page > max_pages,
-            current_page=target_page
+            is_next_page=target_page < max_pages,
+            current_page=target_page,
         )
-            
 
     def get_maximum_amount_of_pages(self) -> int:
         elements: list[Any] = self.db_reader.get_whitelisted_users()
