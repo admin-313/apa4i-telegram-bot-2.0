a49a76ea2e1d6b410c927c6dfaa270b3d1e1d768
admin-313 <w@quantum-level.net>
2025-02-04 10:36:05 +0200

    Add return type

--- src/admin/handlers.py
+++ src/admin/handlers.py
@@ -2,6 +2,7 @@ import logging
 from aiogram.types.message import Message
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 from aiogram.utils.formatting import Code, Text
+from admin.exceptions import BotInstanceNotFound
 from admin.paginator.paginator import Paginator
 from admin.paginator.schemas import PaginatorResponse
 from admin.buttons import (
@@ -16,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 async def respond_to_get(
     message: Message, paginator: Paginator, page: int, is_callback: bool
-) -> None:
+) -> Message:
     response: PaginatorResponse = paginator.get_page(page)
     response_text: Text = Text(
         f"Page {response.current_page} of {response.total_pages}\n",
@@ -37,10 +38,17 @@ async def respond_to_get(
         buttons = get_paginator_first_page_markup(
             next_target_page=response.current_page + 1
         )
-    
+
     if is_callback:
         await message.edit_text(**response_text.as_kwargs())
         await message.edit_reply_markup(reply_markup=buttons)
+        return message
     else:
         if message.bot:
-            await message.bot.send_message(**response_text.as_kwargs(), chat_id=message.chat.id, reply_markup=buttons)
+            return await message.bot.send_message(
+                **response_text.as_kwargs(),
+                chat_id=message.chat.id,
+                reply_markup=buttons,
+            )
+        else:
+            raise BotInstanceNotFound("Could not get bot out of the Message")
