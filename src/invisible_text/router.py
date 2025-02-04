cbfa2eb1eb18c6aaa51ff75d92b2ea20c68596e8
admin-313 <w@quantum-level.net>
2025-02-04 14:51:16 +0200

    Add returns and exception raise for all functions

--- src/invisible_text/router.py
+++ src/invisible_text/router.py
@@ -4,13 +4,14 @@ from aiogram.types.callback_query import CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.utils.formatting import Code, Text
 from invisible_text.callbacks import InvisibleTextCallback
-from src.invisible_text.fsm_states import InvisibleTextForm
+from invisible_text.fsm_states import InvisibleTextForm
 from invisible_text.buttons import (
     get_invisible_text_markup,
     get_invisible_text_on_success,
     get_invisible_text_on_repeat,
 )
 from invisible_text.service import get_unique_text
+from admin.exceptions import MessageInstanceNotFound
 
 invisible_text_router = Router()
 
@@ -18,12 +19,15 @@ invisible_text_router = Router()
 @invisible_text_router.callback_query(
     InvisibleTextCallback.filter(F.action == "get_invisible_text")
 )
-async def invisible_text_callback(callback: CallbackQuery, state: FSMContext) -> None:
+async def invisible_text_callback(callback: CallbackQuery, state: FSMContext) -> Message:
     message = callback.message
     if type(message) is Message:
         await state.set_state(InvisibleTextForm.give_your_text)
         await message.edit_caption("Введите текст для преобразования:")
         await message.edit_reply_markup(reply_markup=get_invisible_text_markup())
+        return message
+    else:
+        raise MessageInstanceNotFound("Could not find the message")
 
 
 @invisible_text_router.callback_query(
@@ -31,7 +35,7 @@ async def invisible_text_callback(callback: CallbackQuery, state: FSMContext) ->
 )
 async def repeat_invisible_text_callback(
     callback: CallbackQuery, state: FSMContext
-) -> None:
+) -> Message:
     message = callback.message
     if type(message) is Message and message.text:
         await state.set_state(InvisibleTextForm.give_your_text)
@@ -39,16 +43,19 @@ async def repeat_invisible_text_callback(
         text_content = Text(Code(message.text), "\n\n", "Введите текст для преобразования")
         await message.edit_text(**text_content.as_kwargs())
         await message.edit_reply_markup(reply_markup=get_invisible_text_on_repeat())
+        return message
+    else:
+        raise MessageInstanceNotFound("Could not find the message")
 
 
 @invisible_text_router.message(InvisibleTextForm.give_your_text, F.text)
-async def return_invisible_test(message: Message, state: FSMContext) -> None:
+async def return_invisible_test(message: Message, state: FSMContext) -> Message:
     if message.text:
         text_content = Text(Code(get_unique_text(message.text)))
-        await message.reply(
+        await state.clear()
+        return await message.reply(
             reply_markup=get_invisible_text_on_success(),
             **text_content.as_kwargs()
         )
-        await state.clear()
     else:
-        await message.reply(text="Введите текст сообщением")
+        return await message.reply(text="Введите текст сообщением")
