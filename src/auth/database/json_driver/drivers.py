bc906a05fad2e6597922754e1f8a007f9ace8d65
admin-313 <w@quantum-level.net>
2025-02-04 15:05:54 +0200

    Add user remover to the json admin

--- src/auth/database/json_driver/drivers.py
+++ src/auth/database/json_driver/drivers.py
@@ -50,12 +50,17 @@ class JSONConfigReader:
 class JSONConfigWriter(JSONConfigReader):
 
     def add_whitelisted_user(self, user: User) -> User:
-        return self._append_to_json_db(user)
+        return self._append_user_to_json_db(user)
         
-    def remove_whitelisted_user_if_exists(self, user: User) -> User:
-        pass
+    def remove_whitelisted_user_if_exists(self, user: User) -> User | None:
+        if self.is_in_database(user.id):
+            users: list[User] = self.get_whitelisted_users()
+            new_users: list[User] = [usr for usr in users if usr.id != user.id]
+            if len(users) != len(new_users):
+                self._rewrite_json_db(users=new_users)
+                return user
 
-    def _append_to_json_db(self, user: User) -> User:
+    def _append_user_to_json_db(self, user: User) -> User:
         try:
             all_users: list[User] = self.get_whitelisted_users()
             all_users.append(user)
@@ -64,6 +69,19 @@ class JSONConfigWriter(JSONConfigReader):
             )
 
             return user
+        except FileNotFoundError:
+            logger.critical(f"The users.json config file doesn't seem to exist")
+            raise WhitelistConfigFileNotFound(
+                "The config file doesn't seem to exist. Have you renamed it to users.json?"
+            )
+        
+    def _rewrite_json_db(self, users: list[User]) -> list[User]:
+        try:
+            new_users: list[User] = users
+            USERS_CONFIG_PATH.write_bytes(
+                TypeAdapter(list[User]).dump_json(new_users, indent=4)
+            )
+            return new_users
         except FileNotFoundError:
             logger.critical(f"The users.json config file doesn't seem to exist")
             raise WhitelistConfigFileNotFound(
