dd98810416c5af8d544401fe985cf90826cfb4e8
admin-313 <w@quantum-level.net>
2025-02-12 12:03:05 +0200

    Change imports to new ORM entity name

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,7 @@ sys.path.append(str(project_root))
 sys.path.append(str(project_root / "src"))
 
 
-from infrastructure.persistence.models import user
+from infrastructure.persistence.models import user_model
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -26,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = user.Base.metadata
+target_metadata = user_model.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
