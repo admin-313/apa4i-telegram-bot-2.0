3ff5bf1477823a236e6bfc60b8b6861405ab2d1b
admin-313 <w@quantum-level.net>
2025-02-12 16:56:37 +0200

    Add comment

--- src/domain/common/interfaces/database.py
+++ src/domain/common/interfaces/database.py
@@ -4,7 +4,14 @@ from typing import Generic, TypeVar, AsyncContextManager
 
 SessionType = TypeVar("SessionType")
 
+
 class Database(ABC, Generic[SessionType]):
+    """So we are using Generic here basically because
+    we may want to have multiple session types based on what db we choose.
+    In our scope it allows for using both the async ans sync alchemy's session types.
+    To pass them we use Database[AsyncSession] or Database[SyncSession]
+    in the infrastructure layer"""
+
     @abstractmethod
     def get_session(self) -> AsyncContextManager[SessionType]:
         pass
