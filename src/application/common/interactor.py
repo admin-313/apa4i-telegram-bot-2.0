02862b02713846da30a6b767adea30746717f203
admin-313 <w@quantum-level.net>
2025-02-13 12:45:10 +0200

    base interactor

---
+++ src/application/common/interactor.py
@@ -0,0 +1,9 @@
+from typing import Generic, TypeVar
+
+InputData = TypeVar("InputData")
+OutputData = TypeVar("OutputData")
+
+
+class Interactor(Generic[InputData, OutputData]):
+    def __call__(self, data: InputData) -> OutputData:
+        raise NotImplementedError
