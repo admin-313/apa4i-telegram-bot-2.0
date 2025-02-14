efd859f6f24a474d289adc0c12c51586044d43f6
admin-313 <w@quantum-level.net>
2025-02-14 17:48:32 +0200

    rm user_id mention and add telegram_id attribute

--- src/domain/models/user.py
+++ src/domain/models/user.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 from datetime import datetime
-from domain.models.user_id import UserId
+
 
 @dataclass
 class User:
-    id: UserId
+    telegram_id: int
     member_since: datetime
     is_superuser: bool
     last_known_name: str | None
