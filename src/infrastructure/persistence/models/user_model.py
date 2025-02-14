e91f00cb5ee8a7810e1b75ed18893db1edad4c62
admin-313 <w@quantum-level.net>
2025-02-14 17:17:09 +0200

    Add telegram_id field to the User table

--- src/infrastructure/persistence/models/user_model.py
+++ src/infrastructure/persistence/models/user_model.py
@@ -10,6 +10,7 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(unique=True, nullable=False)
     member_since: Mapped[datetime]
     is_superuser: Mapped[bool] = mapped_column(nullable=False)
     last_known_name: Mapped[str] = mapped_column(String(23), nullable=True)
