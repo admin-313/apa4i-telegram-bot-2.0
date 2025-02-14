6a17d06124b04c6392dfc2697ae217138f15d034
admin-313 <w@quantum-level.net>
2025-02-14 15:31:44 +0200

    Add autoincrementation to the User's id column

--- src/infrastructure/persistence/models/user_model.py
+++ src/infrastructure/persistence/models/user_model.py
@@ -9,7 +9,9 @@ from infrastructure.persistence.models.base import Base
 class UserModel(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True, nullable=False, autoincrement=True
+    )
     member_since: Mapped[datetime]
     is_superuser: Mapped[bool] = mapped_column(nullable=False)
     last_known_name: Mapped[str] = mapped_column(String(23), nullable=True)
