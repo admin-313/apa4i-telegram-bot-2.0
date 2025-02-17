97c4fc1ba383c3b92ee87729b2f45ac779d8c513
admin-313 <w@quantum-level.net>
2025-02-17 11:40:41 +0200

    Add id attribute

--- src/domain/models/user.py
+++ src/domain/models/user.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 
 @dataclass
 class User:
+    id: Optional[int]
     telegram_id: int
     member_since: datetime
     is_superuser: bool
