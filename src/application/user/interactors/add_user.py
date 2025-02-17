93e7f79ea82c6a41a05ba0107873535603225e4c
admin-313 <w@quantum-level.net>
2025-02-17 18:40:16 +0200

    Add user interactor

--- src/application/user/interactors/add_user.py
+++ src/application/user/interactors/add_user.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Protocol
+from typing import Protocol, Optional
 
 from domain.models.user import User
-from domain. import 
+from domain.services.user import UserService
 from application.common.interactor import Interactor
 from application.user.interfaces.user_repository import UserWriter
 
@@ -13,7 +13,7 @@ class AddUserDTO:
     telegram_id: int
     member_since: datetime
     is_superuser: bool
-    last_known_name: str | None = None
+    last_known_name: Optional[str]
 
 
 class UserWriteRepository(UserWriter, Protocol):
@@ -21,14 +21,16 @@ class UserWriteRepository(UserWriter, Protocol):
 
 
 class AddUser(Interactor[AddUserDTO, None]):
-    def __init__(self, user_writer: UserWriteRepository, user_service: ) -> None:
-        self._user_read_write = user_writer
-        self._user_service = 
+    def __init__(
+        self, user_writer: UserWriteRepository, user_service: UserService
+    ) -> None:
+        self._user_writer = user_writer
+        self._user_service = user_service
+
     async def __call__(self, data: AddUserDTO) -> None:
-        new_user = User(
-            id=None,
+        new_user: User = self._user_service.create_user(
             telegram_id=data.telegram_id,
-            member_since=data.member_since,
             is_superuser=data.is_superuser,
             last_known_name=data.last_known_name,
         )
+        await self._user_writer.add_user(user=new_user)
