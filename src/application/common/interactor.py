fa5a5ed205d94a92707c6875fb46ff5d9bb35518
admin-313 <w@quantum-level.net>
2025-02-18 17:10:58 +0200

    Add interactor factory

--- src/application/common/interactor.py
+++ src/application/common/interactor.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Callable
 
 InputData = TypeVar("InputData")
 OutputData = TypeVar("OutputData")
@@ -7,3 +7,7 @@ OutputData = TypeVar("OutputData")
 class Interactor(Generic[InputData, OutputData]):
     async def __call__(self, data: InputData) -> OutputData:
         raise NotImplementedError
+
+
+InteractorT = TypeVar("InteractorT")
+InteractorFactory = Callable[[], InteractorT]
