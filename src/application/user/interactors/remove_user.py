eb2888a7ef577bb5b4aa1e06e2ebe23f30b127e9
admin-313 <w@quantum-level.net>
2025-02-18 17:39:20 +0200

    remove user use case

---
+++ src/application/user/interactors/remove_user.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Protocol
+from application.common.interactor import Interactor
+from application.user.interfaces.user_repository import UserWriter
+from domain.models.user_id import UserId
+
+
+@dataclass(frozen=True)
+class RemoveUserDTO:
+    user_id: UserId
+
+
+class UserWriteRepository(UserWriter, Protocol):
+    pass
+
+
+class RemoveUser(Interactor[RemoveUserDTO, UserId]):
+    def __init__(self, user_writer: UserWriteRepository) -> None:
+        self._user_writer = user_writer
+
+    async def __call__(self, data: RemoveUserDTO) -> UserId:
+        await self._user_writer.remove_user(data.user_id)
+        return data.user_id
