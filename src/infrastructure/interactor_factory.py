05f2c2c036acce3f5cbb6aeae3a70b56e85a6b0f
admin-313 <w@quantum-level.net>
2025-02-19 15:31:06 +0200

    Fix return type of the abstract methods

--- src/infrastructure/interactor_factory.py
+++ src/infrastructure/interactor_factory.py
@@ -1,4 +1,4 @@
-from typing import ContextManager
+from typing import AsyncContextManager
 from abc import ABC, abstractmethod
 
 from application.user.interactors.add_user import AddUser
@@ -11,25 +11,25 @@ from application.user.interactors.promote_user import PromoteUser
 
 class InteractorFactory(ABC):
     @abstractmethod
-    async def add_user(self) -> ContextManager[AddUser]:
+    async def add_user(self) -> AsyncContextManager[AddUser]:
         raise NotImplementedError
 
     @abstractmethod
-    async def demote_user(self) -> ContextManager[DemoteUser]:
+    async def demote_user(self) -> AsyncContextManager[DemoteUser]:
         raise NotImplementedError
 
     @abstractmethod
-    async def is_admin(self) -> ContextManager[IsAdmin]:
+    async def is_admin(self) -> AsyncContextManager[IsAdmin]:
         raise NotImplementedError
 
     @abstractmethod
-    async def is_whitelisted(self) -> ContextManager[IsWhitelisted]:
+    async def is_whitelisted(self) -> AsyncContextManager[IsWhitelisted]:
         raise NotImplementedError
 
     @abstractmethod
-    async def remove_user(self) -> ContextManager[RemoveUser]:
+    async def remove_user(self) -> AsyncContextManager[RemoveUser]:
         raise NotImplementedError
 
     @abstractmethod
-    async def promote_user(self) -> ContextManager[PromoteUser]:
+    async def promote_user(self) -> AsyncContextManager[PromoteUser]:
         raise NotImplementedError
